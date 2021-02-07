045d7d30bfa6087ef7db32d33d258eab74776225
Tori <croteau.victoria1@gmail.com>
2021-02-07 11:10:49 -0500

    added touch movement, and camera follows player on y axis

---
+++ Mobile Game Project 1/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform targetTransform;
+    Vector3 tempVec3 = new Vector3();
+
+
+    void LateUpdate() //https://gamedev.stackexchange.com/questions/147526/only-make-camera-follow-player-on-x-axis
+    {
+        tempVec3.x = this.transform.position.x;
+        tempVec3.y = targetTransform.position.y;
+        tempVec3.z = this.transform.position.z;
+        this.transform.position = tempVec3;
+
+        gameObject.transform.position = new Vector3
+            (
+             this.transform.position.x,
+             tempVec3.y,
+             this.transform.position.z
+            );
+    }
+}
---
+++ Mobile Game Project 1/Assets/Scripts/MoveByTouch.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveByTouch : MonoBehaviour
+{
+    float screenWidth = Screen.width;
+    public float moveSpeed;
+    private Touch touch;
+    private Vector2 touchPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+            if (touch.position.x > (screenWidth / 2))
+            {
+                //right
+                transform.Translate(moveSpeed, moveSpeed, 0);
+            }
+            else
+            {
+                //left
+                transform.Translate(-moveSpeed, moveSpeed, 0);
+            }
+        }
+        else
+        {
+            transform.Translate(0, moveSpeed, 0);
+        }
+
+        
+        //if (Input.touchCount > 0)
+        //{
+
+
+        //    if (theTouch.phase == TouchPhase.Began)
+        //    {
+        //        touchPosition = theTouch.position;
+        //        float x = touchPosition.x;
+        //        if (Mathf.Abs(x) < 0)
+        //        {
+        //            //left
+        //            transform.Translate(-moveSpeed, 0, 0);
+        //        }
+        //        else if (Mathf.Abs(x) > 0)
+        //        {
+        //            //right
+        //            transform.Translate(moveSpeed, 0, 0);
+        //        }
+        //    }
+        //}
+    }
+
+    //private void FixedUpdate()
+    //{
+    //    TouchMove();
+    //}
+
+    //void TouchMove()
+    //{
+    //    if (Input.GetMouseButton(0))
+    //    {
+    //        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    //        if(mousePos.x > 1)
+    //        {
+    //            //move right
+    //            transform.Translate(moveSpeed, 0, 0);
+    //        }
+    //        if(mousePos.x < -1)
+    //        {
+    //            //move left
+    //            transform.Translate(-moveSpeed, 0, 0);
+    //        }
+
+    //    }
+
+
+    //}
+}
