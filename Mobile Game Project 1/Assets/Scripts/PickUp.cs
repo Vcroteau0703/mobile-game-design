0d1d0487394c21563d7ab070ccaf245d11ca4b81
Tori <croteau.victoria1@gmail.com>
2021-02-12 11:56:59 -0500

    Added Smoother slider transitions

--- Mobile Game Project 1/Assets/Scripts/PickUp.cs
+++ Mobile Game Project 1/Assets/Scripts/PickUp.cs
@@ -5,30 +5,33 @@ using UnityEngine.UI;
 
 public class PickUp : MonoBehaviour
 {
+    public float fillSpeed = 1f;
     public Slider waterSlider;
     public Slider sunSlider;
     public float targetTime = 5f;
-    //public float waterValue;
-    //public float sunValue;
+    public float waterValue;
+    public float sunValue;
+    public float curWaterVal;
+    public float curSunVal;
 
     private void Awake()
     {
-        waterValue = waterSlider.value; 
-        sunValue = sunSlider.value;
+        waterValue = curWaterVal = waterSlider.value; 
+        sunValue = curSunVal = sunSlider.value;
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "water")
         {
-            waterSlider.value += 20;
-            sunSlider.value -= 5;
-            Destroy(collision);
+            waterValue += 20;
+            sunValue -= 5;
+            Destroy(collision.gameObject);
         }
         else if (collision.tag == "sunlight")
         {
-            sunSlider.value += 20;
-            waterSlider.value -= 5;
-            Destroy(collision);
+            sunValue += 20;
+            waterValue -= 5;
+            Destroy(collision.gameObject);
         }
     }
 
@@ -37,10 +40,15 @@ public class PickUp : MonoBehaviour
         targetTime -= Time.deltaTime;
         if(targetTime <= 0.0f)
         {
-            sunSlider.value -= 1;
-            waterSlider.value -= 1;
+            sunValue -= 1;
+            waterValue -= 1;
             targetTime = 1f;
         }
+        curWaterVal = Mathf.Lerp(curWaterVal, waterValue, Time.deltaTime * fillSpeed);
+        curSunVal = Mathf.Lerp(curSunVal, sunValue, Time.deltaTime * fillSpeed);
+
+        waterSlider.value = curWaterVal;
+        sunSlider.value = curSunVal;
     }
 
 
