6609b6e810726162ab6f7034c8632d9801113194
Tori <croteau.victoria1@gmail.com>
2021-02-12 12:31:31 -0500

    added gameover and restart functionality
    there is a strange bug on restart, that I am leaving for now. it seems all scripts attached to the player are broken upon restarting the level

---
+++ Mobile Game Project 1/Assets/Scripts/GameOver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject player;
+    public MoveByTouch moveByTouch;
+    public Slider waterSlider;
+    public Slider sunSlider;
+    public GameObject gameOver;
+
+    private void Awake()
+    {
+        moveByTouch = player.GetComponent<MoveByTouch>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(waterSlider.value == 100 || waterSlider.value == 0)
+        {
+            Time.timeScale = 0;
+            gameOver.SetActive(true);
+            moveByTouch.enabled = false;
+        }
+        if (sunSlider.value == 100 || sunSlider.value == 0)
+        {
+            Time.timeScale = 0;
+            gameOver.SetActive(true);
+            moveByTouch.enabled = false;
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
