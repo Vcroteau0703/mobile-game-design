32f9bca76622fcd80cc5563541301bf4b54e3fe6
Tori <croteau.victoria1@gmail.com>
2021-02-17 16:15:33 -0500

    Fixed bug on restart in the GameOver script

--- Mobile Game Project 1/Assets/Scripts/GameOver.cs
+++ Mobile Game Project 1/Assets/Scripts/GameOver.cs
@@ -11,31 +11,42 @@ public class GameOver : MonoBehaviour
     public Slider waterSlider;
     public Slider sunSlider;
     public GameObject gameOver;
+    private SpawnPickups spawnPickups;
 
     private void Awake()
     {
         moveByTouch = player.GetComponent<MoveByTouch>();
+        spawnPickups = GetComponent<SpawnPickups>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(waterSlider.value == 100 || waterSlider.value == 0)
+        if(waterSlider.value == 100 || waterSlider.value == 0 || sunSlider.value == 100 || sunSlider.value == 0)
         {
-            Time.timeScale = 0;
-            gameOver.SetActive(true);
-            moveByTouch.enabled = false;
+            Pause();
         }
-        if (sunSlider.value == 100 || sunSlider.value == 0)
-        {
-            Time.timeScale = 0;
-            gameOver.SetActive(true);
-            moveByTouch.enabled = false;
-        }
+
+    }
+
+    void Pause()
+    {
+        gameOver.SetActive(true);
+        moveByTouch.enabled = false;
+        spawnPickups.enabled = false;
+    }
+
+    void UnPause()
+    {
+        moveByTouch.enabled = true;
+        spawnPickups.enabled = true;
+        gameOver.SetActive(false);
     }
 
     public void Restart()
     {
+
         SceneManager.LoadScene(0);
+        UnPause();
     }
 }
